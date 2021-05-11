17fd280136b1c6af54807c4446d190245a7e408e
Patchara Chukiatkajohn <patchara@igeargeek.com>
2021-05-11 18:10:16 +0700

    [Edited] newDeck to deck.go

--- basic/deck.go
+++ basic/deck.go
@@ -9,3 +9,22 @@ func (d deck) print() {
 		fmt.Println(card)
 	}
 }
+
+func newDeck() deck {
+	generatedCardDeck := deck{}
+	cardSuits := []string{"Diamonds", "Hearts", "Spades", "Clubs"}
+	cardValues := []string{
+		"Ace",
+		"Two", "Three", "Four", "Five", "Six",
+		"Seven", "Eight", "Nine", "Ten",
+		"Jack", "Queen", "King",
+	}
+
+	for _, suit := range cardSuits {
+		for _, cardValue := range cardValues {
+			generatedCardDeck = append(generatedCardDeck, suit+" of "+cardValue)
+		}
+	}
+
+	return generatedCardDeck
+}
--- basic/main.go
+++ basic/main.go
@@ -1,29 +1,6 @@
 package main
 
 func main() {
-	cards := deck{}
-	cards.print(newDeck())
-}
-
-func newCard() string {
-	return "Ace of Spades"
-}
-
-func newDeck() []string {
-	generatedCardDeck := []string{}
-	cardSuits := []string{"Diamonds", "Hearts", "Spades", "Clubs"}
-	cardValues := []string{
-		"Ace",
-		"Two", "Three", "Four", "Five", "Six",
-		"Seven", "Eight", "Nine", "Ten",
-		"Jack", "Queen", "King",
-	}
-
-	for _, suit := range cardSuits {
-		for _, cardValue := range cardValues {
-			generatedCardDeck = append(generatedCardDeck, suit+" of "+cardValue)
-		}
-	}
-
-	return generatedCardDeck
+	cards := newDeck()
+	cards.print()
 }
