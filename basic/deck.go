d59a4ee51284338b42602f72f0acaf6bfeeef66c
Patchara Chukiatkajohn <patchara@igeargeek.com>
2021-05-13 18:55:09 +0700

    [Edited] learning part3

--- basic/deck.go
+++ basic/deck.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"strings"
+	"time"
+)
 
 type deck []string
+type Rand struct{}
 
 func (d deck) print() {
 	for _, card := range d {
@@ -31,4 +38,29 @@ func newDeck() deck {
 
 func deal(cards deck, handedSize int) (deck, deck) {
 	return cards[:handedSize], cards[handedSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ",")
+}
+
+func (d deck) SaveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0777)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error!", err)
+	}
+	return strings.Split(string(bs), ",")
+}
+
+func (d deck) shuffle() {
+	rSource := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for i := range d {
+		newPosition := rSource.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- basic/main.go
+++ basic/main.go
@@ -1,14 +1,20 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := newDeck()
-	hand, remainingCard := deal(cards, 3)
+	// cards := newDeck()
+	// hand, remainingCards := deal(cards, 3)
 
-	fmt.Println("Hand cards_____:")
-	hand.print()
+	// fmt.Println("Hand cards_____:")
+	// hand.print()
 
-	fmt.Println("Remaining cards_____:")
-	remainingCard.print()
+	// fmt.Println("Remaining cards_____:")
+	// remainingCards.print()
+
+	// if err := cards.SaveToFile("deckInfo.txt"); err != nil {
+	// 	fmt.Println("Error : ", err)
+	// }
+
+	cards := newDeckFromFile("deckInfo.txt")
+	cards.shuffle()
+	cards.print()
 }
