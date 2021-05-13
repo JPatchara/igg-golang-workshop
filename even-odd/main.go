df5018d8e9bb4c8d5dd2f377933782f0d77f201a
Patchara Chukiatkajohn <patchara@igeargeek.com>
2021-05-13 20:29:05 +0700

    [Edited] even odd home work

---
+++ even-odd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for val := range numbers {
+		numberType := ""
+		if val%2 == 0 {
+			numberType = "even"
+		} else {
+			numberType = "odd"
+		}
+
+		fmt.Printf("%d is %s\n", val, numberType)
+	}
+}
