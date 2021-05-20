a6838842f07dc0c3095b849b8ef5feeb3ffd66e5
Patchara Chukiatkajohn <patchara@igeargeek.com>
2021-05-20 18:55:52 +0700

    [Edited] try http pkg

--- interface/main.go
+++ interface/main.go
@@ -1,45 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
-type bot interface {
-	getGreeting() string
-}
+// type bot interface {
+// 	getGreeting() string
+// }
 
-type englishBot struct {
-}
-type spanishBot struct {
-}
-type thaiBot struct {
-}
+// type englishBot struct {
+// }
+// type spanishBot struct {
+// }
+// type thaiBot struct {
+// }
 
 func main() {
-	name := "Jin"
-	eb := englishBot{}
-	sb := spanishBot{}
-	tb := thaiBot{}
+	resp, err := http.Get("https://www.google.com")
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
 
-	printGreeting(eb, name)
-	printGreeting(sb, name)
-	printGreeting(tb, name)
+	fmt.Println(resp.Status)
+	fmt.Println(resp.Header["Content-Type"])
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(body)
+	// name := "Jin"
+	// eb := englishBot{}
+	// sb := spanishBot{}
+	// tb := thaiBot{}
+
+	// printGreeting(eb, name)
+	// printGreeting(sb, name)
+	// printGreeting(tb, name)
 	// printEbGreeting(eb)
 	// printSbGreeting(sb)
 	// printTbGreeting(tb)
 }
 
-func printGreeting(b bot, name string) {
-	fmt.Println(b.getGreeting(), ",", name)
-}
+// func printGreeting(b bot, name string) {
+// 	fmt.Println(b.getGreeting(), ",", name)
+// }
 
-func (eb englishBot) getGreeting() string {
-	return "Hello! Bro."
-}
-func (sb spanishBot) getGreeting() string {
-	return "Hola!"
-}
-func (tb thaiBot) getGreeting() string {
-	return "Swatdee jaa!"
-}
+// func (eb englishBot) getGreeting() string {
+// 	return "Hello! Bro."
+// }
+// func (sb spanishBot) getGreeting() string {
+// 	return "Hola!"
+// }
+// func (tb thaiBot) getGreeting() string {
+// 	return "Swatdee jaa!"
+// }
 
 // func printEbGreeting(eb englishBot) {
 // 	fmt.Println(eb.getEbGreeting())
