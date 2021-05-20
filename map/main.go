22447bd855b973d0585fc6ee6d1e4beacaffe82f
Patchara Chukiatkajohn <patchara@igeargeek.com>
2021-05-20 18:16:37 +0700

    [Edited] test map key & value

--- map/main.go
+++ map/main.go
@@ -8,6 +8,14 @@ func main() {
 		"Jone": 35,
 		"Juy":  99,
 	}
+	numKey := map[int]string{
+		1: "a",
+		2: "b",
+	}
+	boolKey := map[bool]float32{
+		true:  1.111,
+		false: 0.000,
+	}
 	// colors := map[string]string{
 	// 	"red":   "#FF0000",
 	// 	"green": "#00FF00",
@@ -20,6 +28,8 @@ func main() {
 	// colors["blue"] = "#0000FF"
 
 	fmt.Println(ages)
+	fmt.Println(numKey)
+	fmt.Println(boolKey)
 	// updateMap(colors, "black", "#000000")
 	// printMap(colors)
 }
